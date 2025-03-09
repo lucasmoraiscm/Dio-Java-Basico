af9ad3b97fbd277d4cfb10b32ee92f55a052a889
Lucas Morais <lukazmorayz@gmail.com>
2025-03-08 21:22:33 -0300

    :sparkles:feat: adiciona escola

---
+++ programacao_orientada_a_objetos/fundamentos/escola/src/escola/Aluno.java
@@ -0,0 +1,22 @@
+package escola;
+
+public class Aluno {
+    private String nome;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String novoNome) {
+        this.nome = novoNome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int novaIdade) {
+        this.idade = novaIdade;
+    }
+}

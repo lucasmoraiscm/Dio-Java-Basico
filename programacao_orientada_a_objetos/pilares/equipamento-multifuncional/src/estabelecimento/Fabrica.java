07b96eba577a9759fc07848b95d28c6410a8da51
Lucas Morais <lukazmorayz@gmail.com>
2025-03-10 20:05:34 -0300

    :sparkles:feat: adiciona equipamento-multifuncional

---
+++ programacao_orientada_a_objetos/pilares/equipamento-multifuncional/src/estabelecimento/Fabrica.java
@@ -0,0 +1,29 @@
+package estabelecimento;
+
+import equipamentos.copiadora.Copiadora;
+import equipamentos.digitalizadora.Digitalizadora;
+import equipamentos.impressora.Deskjet;
+import equipamentos.impressora.Impressora;
+import equipamentos.impressora.Laserjet;
+import equipamentos.multifuncional.EquipamentoMultifuncional;
+
+public class Fabrica {
+    public static void main(String[] args) {
+        EquipamentoMultifuncional em = new EquipamentoMultifuncional();
+        em.imprimir();
+
+        Impressora impressoraDeskjet = new Deskjet();
+        impressoraDeskjet.imprimir();
+
+        Impressora impressoraLaserjet = new Laserjet();
+        impressoraLaserjet.imprimir();
+
+        Impressora impressora = em;
+        Digitalizadora digitalizadora = em;
+        Copiadora copiadora = em;
+
+        impressora.imprimir();
+        digitalizadora.digitalizar();
+        copiadora.copiar();
+    }
+}
